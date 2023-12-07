f9f4f300a5fa686e8c8a494f5ec049973cec1f40
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-12-07 16:22:48 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $title = 'Hello World';
+    $title_sub = 'Laravel Primi Passi';
+    $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem officiis necessitatibus atque repudiandae delectus dicta earum ratione aut sed similique nihil illum quos laborum, aliquam quo repellendus numquam iure. Saepe.';
+    return view('home', compact('title', 'title_sub', 'text'));
+})->name('home');
